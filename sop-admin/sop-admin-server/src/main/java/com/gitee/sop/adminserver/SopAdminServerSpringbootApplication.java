6638661b29ee860eecacff52bb454328bdf93b85
tanghc <thc8719@163.com>
2019-03-12 21:13:43 +0800

    admin

---
+++ sop-admin/sop-admin-server/src/main/java/com/gitee/sop/adminserver/SopAdminServerSpringbootApplication.java
@@ -0,0 +1,12 @@
+package com.gitee.sop.adminserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SopAdminServerSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SopAdminServerSpringbootApplication.class, args);
+	}
+}

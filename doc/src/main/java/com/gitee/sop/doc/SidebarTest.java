a5eb49ab6efa6d7ab0631be9d8d4f9fe2f4a88a2
tanghc <thc8775@163.com>
2019-03-23 18:08:21 +0800

    编写文档

--- doc/src/main/java/com/gitee/sop/doc/SidebarTest.java
+++ doc/src/main/java/com/gitee/sop/doc/SidebarTest.java
@@ -2,6 +2,10 @@ package com.gitee.sop.doc;
 
 import java.io.File;
 import java.io.FileOutputStream;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 /**
  * 生成_sidebar.md文件，直接运行即可
@@ -18,8 +22,12 @@ public class SidebarTest {
 		String fileDir = root + "/docs/files";
 		File dir = new File(fileDir);
 		File[] files = dir.listFiles();
+		Stream<File> filesStream = Stream.of(files);
+		List<File> fileList = filesStream
+				.sorted(Comparator.comparing(File::getName))
+				.collect(Collectors.toList());
 		StringBuilder output = new StringBuilder();
-		for (File file : files) {
+		for (File file : fileList) {
 			String filename = file.getName();
 			String title = filename.substring(filename.indexOf("_") + 1, filename.length() - 3);
 			String line = String.format(format, title, filename, System.currentTimeMillis());

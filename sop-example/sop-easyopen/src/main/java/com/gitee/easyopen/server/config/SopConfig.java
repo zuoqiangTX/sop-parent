77cfec981c807c4a9df9d2af642ba7b15a0472e6
tanghc <thc8775@163.com>
2019-03-28 20:45:46 +0800

    添加easyopen例子

---
+++ sop-example/sop-easyopen/src/main/java/com/gitee/easyopen/server/config/SopConfig.java
@@ -0,0 +1,11 @@
+package com.gitee.easyopen.server.config;
+
+import com.gitee.sop.servercommon.configuration.EasyopenServiceConfiguration;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author tanghc
+ */
+@Configuration
+public class SopConfig extends EasyopenServiceConfiguration {
+}

800f7e8d82c8d9af90764ad19663e0ada9f3e734
tanghc <thc8775@163.com>
2019-06-27 19:07:33 +0800

    秘钥管理改造，服务端返回sign

--- sop-gateway/src/main/java/com/gitee/sop/gateway/mapper/IsvInfoMapper.java
+++ sop-gateway/src/main/java/com/gitee/sop/gateway/mapper/IsvInfoMapper.java
@@ -1,11 +1,26 @@
 package com.gitee.sop.gateway.mapper;
 
 import com.gitee.fastmybatis.core.mapper.CrudMapper;
+import com.gitee.sop.gateway.entity.IsvDetailDTO;
 import com.gitee.sop.gateway.entity.IsvInfo;
+import org.apache.ibatis.annotations.Select;
+
+import java.util.List;
 
 
 /**
  * @author tanghc
  */
 public interface IsvInfoMapper extends CrudMapper<IsvInfo, Long> {
+
+    @Select("SELECT  " +
+            "  t.app_key appKey " +
+            "  ,t.status " +
+            "  ,t2.sign_type signType " +
+            "  ,t2.secret " +
+            "  ,t2.public_key_isv publicKeyIsv " +
+            "  ,t2.private_key_platform privateKeyPlatform " +
+            "FROM isv_info t " +
+            "INNER JOIN isv_keys t2 ON t.app_key = t2.app_key")
+    List<IsvDetailDTO> listIsvDetail();
 }

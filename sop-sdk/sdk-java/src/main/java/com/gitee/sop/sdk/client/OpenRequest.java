5231d4bc171f4d3d02108fa7e5592245fdaa194a
tanghc <thc8775@163.com>
2019-08-14 12:01:04 +0800

    1.15.0

--- sop-sdk/sdk-java/src/main/java/com/gitee/sop/sdk/client/OpenRequest.java
+++ sop-sdk/sdk-java/src/main/java/com/gitee/sop/sdk/client/OpenRequest.java
@@ -42,7 +42,7 @@ public class OpenRequest {
                     }
                     return openHttp.get(url, header);
                 } else {
-                    return openHttp.request(url, form, header, OpenHttp.HTTPMethod.fromValue(requestMethod.name()));
+                    return openHttp.request(url, form, header, requestMethod);
                 }
             }
         } catch (IOException e) {
